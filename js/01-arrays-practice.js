165b4b3d6617210864cd313711be1a87fa017399
jumpman22 <hiram.cho@gmail.com>
2018-03-01 21:17:36 -0800

    part01

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,35 +1,153 @@
+/* eslint-env browser */
 //STEP 1
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King'];
+//window.console.log(movies[1]);
 //STEP 2
-
+//var movies = new Array(5);
+//movies[0] = ('Top Gun');
+//movies[1] = ('Rocky');
+//movies[2] = ('Coco');
+//movies[3] = ('Alien');
+//movies[4] = ('Transformers');
+//window.console.log(movies[0]);
 //STEP 3
-
+//var movies = new Array(5);
+//movies[0] = ('Top Gun');
+//movies[1] = ('Rocky');
+//movies[2] = ('Coco');
+//movies[3] = ('Alien');
+//movies[4] = ('Transformers');
+//movies.splice(4, 0, 'Armageddon');
+//window.console.log(movies.length);
 //STEP 4
-
+//var movies = [];
+//movies[0] = ('Top Gun');
+//movies[1] = ('Rocky');
+//movies[2] = ('Coco');
+//movies[3] = ('Alien');
+//movies[4] = ('Transformers');
+//movies.shift();
+//windows.cosole.log(movies);
 //STEP 5
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//for(var i = 0; i <= movies.length; i++) {
+//    window.console.log(movies[i]);
+//}
 //STEP 6
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var x;
+//for(x in movies) {
+//    'use strict';
+//    window.console.log(movies.[x]);
+//}
 //STEP 7
-
+var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+var x;
+for(x in movies) {
+    'use strict';
+    window.console.log(movies.sort());
+}
 //STEP 8
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var leastFavMovies = ['Sex and the City', '50 Shades of Grey', 'Knocked Up'];
+//window.console.log('Movies I like:\n\n');
+//for(var i = 0; i <= movies.length; i++) {
+//    window.console.log(movies[i]);
+//}
+//window.console.log('...\n\n' + 'Movies I regret watching:\n\n');
+//for(x in leastFavMovies) {
+//    'use strict';
+//    window.console.log(leastFavMovies[x]);
+//}
+//window.console.log('...');
 //STEP 9
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var leastFavMovies = ['Sex and the City', '50 Shades of Grey', 'Knocked Up'];
+//movies = movies.concat(leastFavMovies);
+//movies.sort();
+//window.console.log(movies.reverse());
 //STEP 10
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var leastFavMovies = ['Sex and the City', '50 Shades of Grey', 'Knocked Up'];
+//movies = movies.concat(leastFavMovies);
+//movies.sort();
+//movies.reverse();
+//window.console.log(movies.pop());
 //STEP 11
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var leastFavMovies = ['Sex and the City', '50 Shades of Grey', 'Knocked Up'];
+//movies = movies.concat(leastFavMovies);
+//movies.sort();
+//movies.reverse();
+//window.console.log(movies.shift());
 //STEP 12
-
+//var movies = ['Top Gun', 'Karate Kid', 'Rocky', 'Godfather', 'Lion King', 'American Me', 'Casino'];
+//var leastFavMovies = ['Sex and the City', '50 Shades of Grey', 'Knocked Up'];
+//movies = movies.concat(leastFavMovies);
+//movies.sort();
+//movies.reverse();
+//movies.indexOf('Sex and the City');
+//movies.indexOf('Knocked Up');
+//movies.indexOf('50 Shades of Grey');
+//movies[1] = 'Goonies';
+//movies[4] = 'Batman';
+//movies[9] = 'Weekend at Bernies';
 //STEP 13
+//var employee1 = [];
+//employee1["id"] = 934;
+//employee1["name"] = "Lucy";
+//employee1["title"] = "CEO";
+//employee1["department"] = "Marketing";
+//
+//var employee2 = [];
+//employee2["id"] = 876;
+//employee2["name"] = "Mike";
+//employee2["title"] = "Janitor";
+//employee2["department"] = "Operations";
+//window.console.log(employee2.name);
 
 //STEP 14
+var employee1 = [];
+employee1["id"] = 934;
+employee1["name"] = "Lucy";
+employee1["title"] = "CEO";
+employee1["department"] = "Marketing";
 
+
+var employee2 = [];
+employee2["id"] = 876;
+employee2["name"] = "Mike";
+employee2["title"] = "Janitor";
+employee2["department"] = "Operations";
+for 
+window.console.log(employee2.name);
 //STEP 15
+var employee1 = [];
+employee1["id"] = 934;
+employee1["name"] = "Lucy";
+employee1["title"] = "CEO";
+employee1["department"] = "Marketing";
+employee["employed"] = true;
 
+var employee2 = [];
+employee2["id"] = 876;
+employee2["name"] = "Mike";
+employee2["title"] = "Janitor";
+employee2["department"] = "Operations";
+employee["employed"] = true;
+
+var employee2 = [];
+employee2["id"] = 876;
+employee2["name"] = "Mike";
+employee2["title"] = "Janitor";
+employee2["department"] = "Operations";
+employee["employed"] = false;
 //STEP 16
-
+var favMovies = [["Godfather", 5],["Friday Night Lights", 4],["American Me", 1],["Coco", 3], ['Casino', 5]];
+var titles = favMovies.filer(function(item) {
+    'use strict';
+    return typeof === "string";
+});
 //STEP 17
 
 //STEP 18
@@ -37,3 +155,6 @@
 //STEP 19
 
 //STEP 20
+function max(arr) {
+    
+}
