0efc5fc30dc124b9ee33636850e59019bdaf01dc
jumpman22 <hiram.cho@gmail.com>
2018-03-08 16:59:21 -0800

    update assignment

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -93,68 +93,109 @@ for(x in movies) {
 //movies[4] = 'Batman';
 //movies[9] = 'Weekend at Bernies';
 //STEP 13
-//var employee1 = [];
-//employee1["id"] = 934;
-//employee1["name"] = "Lucy";
-//employee1["title"] = "CEO";
-//employee1["department"] = "Marketing";
-//
-//var employee2 = [];
-//employee2["id"] = 876;
-//employee2["name"] = "Mike";
-//employee2["title"] = "Janitor";
-//employee2["department"] = "Operations";
-//window.console.log(employee2.name);
+// var employee1 = [];
+// employee1["id"] = 934;
+// employee1["name"] = "Lucy";
+// employee1["title"] = "CEO";
+// employee1["department"] = "Marketing";
+
+// var employee2 = [];
+// employee2["id"] = 876;
+// employee2["name"] = "Mike";
+// employee2["title"] = "Janitor";
+// employee2["department"] = "Operations";
+
+// var employees = [employee1, employee2];
+// window.console.log(employees[1].name);
 
 //STEP 14
-var employee1 = [];
-employee1["id"] = 934;
-employee1["name"] = "Lucy";
-employee1["title"] = "CEO";
-employee1["department"] = "Marketing";
+// var employee1 = [];
+// employee1["id"] = 934;
+// employee1["name"] = "Lucy";
+// employee1["title"] = "CEO";
+// employee1["department"] = "Marketing";
 
+// var employee2 = [];
+// employee2["id"] = 876;
+// employee2["name"] = "Mike";
+// employee2["title"] = "Janitor";
+// employee2["department"] = "Operations";
 
-var employee2 = [];
-employee2["id"] = 876;
-employee2["name"] = "Mike";
-employee2["title"] = "Janitor";
-employee2["department"] = "Operations";
-for 
-window.console.log(employee2.name);
+// var employees = [employee1, employee2];
+
+// for(var i = 0; i < employees.length; i++) {
+// 	console.log(employees[i].name);
+// }
 //STEP 15
 var employee1 = [];
 employee1["id"] = 934;
 employee1["name"] = "Lucy";
 employee1["title"] = "CEO";
 employee1["department"] = "Marketing";
-employee["employed"] = true;
+employee1["employed"] = true;
 
 var employee2 = [];
 employee2["id"] = 876;
 employee2["name"] = "Mike";
 employee2["title"] = "Janitor";
 employee2["department"] = "Operations";
-employee["employed"] = true;
+employee2["employed"] = true;
 
-var employee2 = [];
-employee2["id"] = 876;
-employee2["name"] = "Mike";
-employee2["title"] = "Janitor";
-employee2["department"] = "Operations";
-employee["employed"] = false;
+var employee3 = [];
+employee3["id"] = 876;
+employee3["name"] = "Sarah";
+employee3["title"] = "Accountant";
+employee3["department"] = "Finance";
+employee3["employed"] = false;
+
+var employees = [employee1, employee2, employee3];
+
+for(var i = 0; i < employees.length; i++) {
+	if(employees[i].employed === true)
+	console.log(employees[i].name);
+}
 //STEP 16
 var favMovies = [["Godfather", 5],["Friday Night Lights", 4],["American Me", 1],["Coco", 3], ['Casino', 5]];
-var titles = favMovies.filer(function(item) {
-    'use strict';
-    return typeof === "string";
+var titles = favMovies.filter(function(title) {
+	return (typeof title === 'string');
 });
+window.console.log(titles);
+
 //STEP 17
-
+// var employees = ['Zak', 'Jessica', 'Mark', 'Fred', 'Sally'];
+// var showEmployee = function(x) {
+// 'use strict';		
+// 	console.log('Employees:\n\n');
+// 	for(var i = 0; i < x.length; i++) {
+// 		window.console.log(x[i]);
+// 	}
+// }
+// showEmployee(employees);
 //STEP 18
-
-//STEP 19
-
-//STEP 20
-function max(arr) {
-    
+function filterValues(arr) {
+	'use strict';
+	var newArr = [];
+	var num = [58, '', 'abcd', true, null, false, 0];
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] === false || arr[i] === null || arr[i] === 0 || arr[i] === '') {
+			newArr.pop(arr[i]);
+		} 
+		console.log(newArr(arr[i]));
+	}
 }
+//STEP 19
+// function random(arr) {
+// 	var randomNum = Math.floor(Math.random()*arr.length);
+// 	window.console.log(arr[randomNum]);
+// }
+//STEP 20
+// function max(arr){
+// 	'use strict';
+// 	var max = arr[0];
+// 	for(var i = 1; i < arr.length; i++){
+// 		if(arr[i] > max){
+// 			max = arr[i];
+// 		}
+// 	}
+// 	return max;
+// }
